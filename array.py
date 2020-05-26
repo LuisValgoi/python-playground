22ac9aa3359c7bb76b987a2948b60697f5ecf4b0
Luis Valgoi <luis.valgoi@sap.com>
2020-05-26 19:16:37 -0300

    feat: added more things to array

--- array.py
+++ array.py
@@ -7,3 +7,22 @@ print(name[1:])
 print(name[-1])
 print(name[0])
 print(name[1:-1])
+
+numbers = [5, 22, 23, 34, 56]
+print(numbers.index(5))
+numbers.insert(len(numbers), 20)
+numbers.clear()
+numbers = [1, 21, 32, 41, 35, 45, 55]
+numbers.pop()
+print(f"Records: {numbers}. Last's Position: {numbers.index(1)}")
+print(1 in numbers)
+print(f'occurrences of 1: {numbers.count(1)}')
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+letters = ['a', 'b', 'c']
+a, b, c = letters
+print(a)
+print(b)
+print(c)
