dc0e15c37f3f1b0725b949dc5bef213091c9aeb0
Luis Valgoi <luis.valgoi@sap.com>
2020-05-26 23:00:12 -0300

    refactor: modules and libs

--- libs.py
+++ libs.py
@@ -1,7 +1,9 @@
 #https://docs.python.org/3/library/math.html
 import math
 import spacer
+from spacer import give_less_spaces
 
 print(math.factorial(5))
 spacer.give_space()
+give_less_spaces()
 print(math.factorial(50))
--- spacer.py
+++ spacer.py
@@ -1,2 +1,6 @@
 def give_space():
     print('-----------------')
+
+
+def give_less_spaces():
+    print('-')
