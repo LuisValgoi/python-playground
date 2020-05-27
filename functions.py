9a825dae57e9f42791d360f78ab777904c41fac1
Luis Valgoi <luis.valgoi@sap.com>
2020-05-26 22:25:40 -0300

    feat: added function examples

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
+# normal arguments
 def callMe():
     print('my name')
-
 print('start')
 callMe()
+
+# position arguments
+def get_name(first, last):
+    return f'{last}, {first}'
+full_name = get_name(first='luis', last='valgoi')
+print(full_name)
+
+# none return
+def return_none():
+    return None
+print(return_none())
