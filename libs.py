26a58ae7b849d19c9dc6195dd9adc6c351a779ff
Luis Valgoi <luis.valgoi@sap.com>
2020-05-26 23:12:11 -0300

    feat: modules and packages

--- libs.py
+++ libs.py
@@ -1,9 +1,11 @@
 #https://docs.python.org/3/library/math.html
 import math
 import spacer
+from ecommerce import shipping
 from spacer import give_less_spaces
 
 print(math.factorial(5))
 spacer.give_space()
 give_less_spaces()
 print(math.factorial(50))
+shipping.calc_shipping()
